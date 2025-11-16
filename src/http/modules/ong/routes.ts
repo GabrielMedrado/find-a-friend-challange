48c61cf5887ed7828e15461f2169930648887848
Gabriel Medrado <medradoogabriel@gmail.com>
2025-11-16 15:11:49 -0300

    fix() adjust roles on routes

--- src/http/modules/ong/routes.ts
+++ src/http/modules/ong/routes.ts
@@ -2,8 +2,9 @@ import { FastifyInstance } from 'fastify';
 import { createOngController } from './controller/create-ong-controller';
 import { authOngController } from './controller/auth-ong-controller';
 import { verifyUserRole } from '@/http/middlewares/verify-user-role';
+import { verifyJWT } from '@/http/middlewares/verify-jwt';
 
 export function ongRoutes(app: FastifyInstance) {
-  app.post('/ong', {onRequest: verifyUserRole('ADMIN')}, createOngController);
-  app.post('/ong/auth', {onRequest: verifyUserRole('ONG')}, authOngController);
+  app.post('/ong', {onRequest: [verifyJWT, verifyUserRole('ADMIN')]}, createOngController);
+  app.post('/ong/auth', authOngController);
 }
--- src/http/modules/pets/routes.ts
+++ src/http/modules/pets/routes.ts
@@ -1,12 +1,10 @@
 import { FastifyInstance } from 'fastify';
 import { createPetController } from './controller/create-pet-controller';
 import { verifyUserRole } from '@/http/middlewares/verify-user-role';
-import { verifyJWT } from '@/http/middlewares/verify-jwt';
 import { getPetByIdController } from './controller/get-pet-by-id-controller';
+import { verifyJWT } from '@/http/middlewares/verify-jwt';
 
-export function petRoutes(app: FastifyInstance){
-  app.addHook('onRequest', verifyJWT);  
-  
-  app.post('/pet', {'onRequest': verifyUserRole('ADMIN')}, createPetController);
+export function petRoutes(app: FastifyInstance){  
+  app.post('/pet', {onRequest: [verifyJWT, verifyUserRole('ONG')]}, createPetController);
   app.get('/pet/:id', getPetByIdController);
 }

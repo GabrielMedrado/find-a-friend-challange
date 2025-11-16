0b5177c459835dc3e0cc8f563c690971f3f27869
Gabriel Medrado <medradoogabriel@gmail.com>
2025-11-16 14:03:29 -0300

    feat(): new features

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,31 @@
 import fastify from 'fastify';
-import { ZodError } from 'zod';
-import { env } from './env';
-import { ongRoutes } from './http/modules/ong/routes';
 import fastifyJwt from '@fastify/jwt';
+import fastifyCookie from '@fastify/cookie';
+import { env } from './env';
+import { ZodError } from 'zod';
+
+import { ongRoutes } from './http/modules/ong/routes';
 import { petRoutes } from './http/modules/pets/routes';
+import { usersRoutes } from './http/modules/users/routes';
 
 export const app = fastify();
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 });
+
+app.register(fastifyCookie);
 
 app.register(ongRoutes);
 app.register(petRoutes);
+app.register(usersRoutes);
 
 
 app.setErrorHandler((error, _, reply) => {
